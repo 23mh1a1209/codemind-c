bd16408f8db2607f7e5114e85f04b58e3b3ad946
23mh1a1209 <147488774+23mh1a1209@users.noreply.github.com>
2024-01-20 17:09:43 +0530

    #include<stdio.h> int main() {     int i,j,n,k;     scanf("%d",&n);     for(i=n;i>=1;i--){         for(k=n-1;k>=i;k--){             printf(" ");         }         for(int j=i;j>=1;j--){             printf("%d ",j);         }         printf(" ");     }     return 0; }

---
+++ Pattern_7.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,k;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){
+        for(k=n-1;k>=i;k--){
+            printf(" ");
+        }
+        for(int j=i;j>=1;j--){
+            printf("%d ",j);
+        }
+        printf("
+");
+    }
+    return 0;
+}
